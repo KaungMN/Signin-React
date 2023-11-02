231d37967a5ec3fb43a453f500713cf7b2bcd772
KaungMN <kaungmyatnoe2016@gmail.com>
2023-11-02 22:35:04 +0630

    [Add] Car Class

--- src/Car.jsx
+++ src/Car.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 
 export default class Car extends Component {
+  constructor(props) {
+    console.log(props);
+    super(props);
+    this.state = { engine: "V8" };
+  }
   render() {
     const { name } = this.props;
     const { color } = this.props;
     const { country } = this.props;
+    const handleChangeEngine = () => this.setState({ engine: "B12" });
     return (
-      <div>
+      <div onClick={handleChangeEngine}>
         This car is {name}.
         <br />
         Its color is {color}.
         <br />
         The manufacturer is {country}.
+        <br />
+        Its engine is {this.state.engine}.
       </div>
     );
   }
